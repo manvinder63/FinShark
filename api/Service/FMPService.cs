41ddc6ec9005b0fe877b68a33f38dea0e1ae447f
manvinder63 <manvinder1963@outlook.com>
2025-07-08 13:18:28 +1000

    Fixed bug(Fetching stocks from financial modeling prep if not present in database)

---
+++ api/Service/FMPService.cs
@@ -0,0 +1,46 @@
+using api.Dtos.Stock;
+using api.Interfaces;
+using api.Mappers;
+using api.Models;
+using Newtonsoft.Json;
+
+namespace api.Service
+{
+    public class FMPService : IFMPService
+    {
+        private HttpClient _httpClient;
+        private readonly IConfiguration _config;
+
+        public FMPService(HttpClient httpClient, IConfiguration config)
+        {
+            _httpClient = httpClient;
+            _config = config;
+        }
+
+        public async Task<Stock> FindStockBySymbolAsync(string symbol)
+        {
+            try
+            {
+                var result = await _httpClient.GetAsync($"https://site.financialmodelingprep.com/api/v3/profile/{symbol}?apiKey={_config["FMPKey"]}");
+                if (result.IsSuccessStatusCode)
+                {
+                    var content = await result.Content.ReadAsStringAsync();
+                    var tasks = JsonConvert.DeserializeObject<FMPStock[]>(content);
+                    var stock = tasks[0];
+                    if (stock != null)
+                    {
+                        return stock.ToStockFromFMP();
+                    }
+
+                    return null;
+                }
+                return null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+    }
+}
